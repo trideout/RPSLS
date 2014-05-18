22b9b5bc90227deba4009e77564fe2339ec58f12
Thomas Rideout <tom@latouraineinc.com>
2014-05-18 16:37:34 -0700

    initial revision

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+
+class HomeController extends BaseController {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Default Home Controller
+	|--------------------------------------------------------------------------
+	|
+	| You may wish to use controllers instead of, or in addition to, Closure
+	| based routes. That's great! Here is an example controller method to
+	| get you started. To route to this controller, just add the route:
+	|
+	|	Route::get('/', 'HomeController@showWelcome');
+	|
+	*/
+
+	public function showChooser()
+	{
+		return View::make('choser');
+	}
+
+    public function throwAction($action)
+    {
+        $judge = new \RPSLS\Referee();
+        $player = new \RPSLS\Player();
+        $myAction = $player->chooseAction();
+        $results = $judge->judgeResults($action, $myAction);
+        return View::make('choser')->with(['results'=>$results, 'yourChoice'=>$action,'myChoice'=>$myAction]);
+    }
+
+    public function testResults()
+    {
+        $judge = new \RPSLS\Referee();
+        $results = $judge->judgeResults('paper', 'paper');
+        dd($results);
+    }
+
+}
